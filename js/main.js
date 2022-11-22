c5f14882168ce5a41b99626d4ee3e92fe7f73de0
wallofwolf <77008335+wallofwolf@users.noreply.github.com>
2022-11-22 09:11:08 +0900

    feat : 로그인 / 회원가입 / 로그인-회원가입 간 토글 기능 추가

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-import { handleLocation } from './router.js';
+import { handleLocation } from "./router.js";
+//
+import { onToggle, handleAuth } from "./login.js";
 
-window.addEventListener('DOMContentLoaded', handleLocation);
-window.addEventListener('hashchange', handleLocation);
+window.addEventListener("DOMContentLoaded", handleLocation);
+window.addEventListener("hashchange", handleLocation);
+
+//
+window.onToggle = onToggle;
+window.handleAuth = handleAuth;
