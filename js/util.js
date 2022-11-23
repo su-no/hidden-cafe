155b8c81b3b12f670281974bc2bd591284206676
Hyojin <0910.shj@gmail.com>
2022-11-23 14:40:08 +0900

    Fix: getDate 함수 포맷팅 수정

--- js/util.js
+++ js/util.js
@@ -1,10 +1,10 @@
 // 2022-11-11 13:52 포맷으로 날짜를 받아오는 함수
 export const getDate = () => {
   const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  const hour = date.getHours();
-  const minuites = date.getMinutes();
+  const year = String(date.getFullYear()).padStart(4, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hour = String(date.getHours()).padStart(2, "0");
+  const minuites = String(date.getMinutes()).padStart(2, "0");
   return `${year}-${month}-${day} ${hour}:${minuites}`;
 };
