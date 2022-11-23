62d243ba20f87792c78ff2fa610e1ea18ddb1fd8
Hyojin <88768022+su-no@users.noreply.github.com>
2022-11-23 23:53:58 +0900

    Refactor: getFirebaseDocs 함수 분리

--- js/all-post.js
+++ js/all-post.js
@@ -89,53 +89,36 @@ export const getPostByLocal = async (local) => {
     };
     postObjList.push(postObj);
   });
+
+  // 게시글 DOM들을 추가할 컨테이너
   const postList = document.getElementById("container");
-  // const currentUid = authService.currentUser.uid;
-  postList.innerHTML = "";
-  postObjList.forEach((postObj) => {
-    // const isOwner = currentUid === postObj.creatorId;
-    const temp_html = `<article class="post">
-    <!-- 게시글 헤더 -->
-    <div class="post-header">
-      <!-- 사용자 정보 -->
-      <div class="post-user">
-        <img
-          class="post-profile-img"
-          src=${postObj.profilImg ?? "/img/profile-img.png"}
-          alt="profile-img"
-        />
-        <div class="post-user-name">${
-          postObj.nickname ?? postObj.email.split("@")[0]
-        }</div>
+
+  // 게시글 데이터가 담겨 있는 배열을 돌면서, 컨테이너에 돔을 추가한다.
+  postObjList.forEach(postObj => {
+    const temp_html = `
+    <article class="post">
+      <div class="post-header">
+        <div class="post-user">
+          <img
+            class="post-profile-img"
+            src=${postObj.profilImg ?? "/img/profile-img.png"}
+            alt="profile-img"
+          />
+          <div class="post-user-name">${postObj.nickname ?? postObj.email.split("@")[0]}</div>
+        </div>
+        <div class="post-create-date">${postObj.createdAt}</div>
       </div>
-      <!-- 작성 날짜 -->
-      <div class="post-create-date">${postObj.createdAt}</div>
-    </div>
-    <!-- 게시글 본문 -->
-    <div class="post-box">
-      
-      <!-- 내용 -->
-      <div class="post-container">
-        <!-- 이미지 -->
-        <img class="post-img" src=${postObj.postImg} alt="post-img" />
-        <div class="post-content">
-          <!-- 제목 -->
-          <a href="#view-post-${postObj.postId}"><h2 class="title">${
-      postObj.title
-    }</h2></a>
-          <!-- 설명 -->
-          <div class="description">
-            ${postObj.contents}
+      <div class="post-box">
+        <div class="post-container">
+          <img class="post-img" src=${postObj.postImg} alt="post-img" />
+          <div class="post-content">
+            <a href="#view-post-${postObj.postId}"><h2 class="title">${postObj.title}</h2></a>
+            <div class="description">${postObj.contents}</div>
           </div>
         </div>
+        <div class="bookmark"><i class="fas fa-mug-hot"></i>${postObj.bookmark}</div>
       </div>
-      <!-- 북마크 버튼 -->
-      <div class="bookmark"><i class="fas fa-mug-hot"></i>${
-        postObj.bookmark
-      }</div>
-
-    </div>
-  </article>`;
+    </article>`;
 
     const div = document.createElement("div");
     div.classList.add("mycards");
